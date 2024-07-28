a34ac3c1fce4b8e81566562a5ca6e3252142d4e4
Pamod460 <pamodkaushika@gmail.com>
2024-07-28 17:30:49 +0530

    set env

--- PMS_Client/src/app/services/shered/ApiConfig.ts
+++ PMS_Client/src/app/services/shered/ApiConfig.ts
@@ -1,7 +1,10 @@
+import {environment} from "../../../environments/environment";
+
 export class ApiConfig{
   //static readonly API_URL="https://sarathhemachandra.com/api"
   //  static readonly API_URL="http://127.0.0.1:5000"
-   static readonly API_URL="http://localhost/PMS_Server"
+  static readonly API_URL = environment.API_URL;
+   // static readonly API_URL="http://localhost/PMS_Server"
   static createURL(query:String){
     return`${this.API_URL}/${query}`
   }
---
+++ PMS_Client/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  API_URL: 'https://patient-management-system-murex.vercel.app/PMS_Server'
+};
